4e7fd0e68cdb683618cb5b3d9f999a8918a349b5
lywo <lydia.wolfs@student.uva.nl>
2016-05-08 12:19:39 +0200

    End of day -final

--- app/src/main/java/com/example/lydia/lydiawolfs_pset3/MyAdapter.java
+++ app/src/main/java/com/example/lydia/lydiawolfs_pset3/MyAdapter.java
@@ -5,16 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.TextView;
-
-import com.example.lydia.lydiawolfs_pset3.R;
-
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Lydia on 29-4-2016.
+ * MyAdapter over ListView with to do items.
+ * ToDO make personal & add checboxes to checkoff any todo without deleten them.
  */
 
 public class MyAdapter extends ArrayAdapter<String>{
